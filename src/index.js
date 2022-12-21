397e1bfb278ebdf557edde13fb568d82ebbed671
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-21 22:17:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const refs = {
 const DEBOUNCE_DELAY = 300;
 
 function fetchCountries(name) {
-    return fetch("https://restcountries.com/v3.1/name/${name}?fields=name,capital,currencies,population,flags,lenguages").then(response => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,currencies,population,flags,lenguages`).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
@@ -20,15 +20,88 @@ function fetchCountries(name) {
     })
 }
 
-refs.info.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput (event){
     event.preventDefault();
 
-    inputCountry = event.target.value.trim();
+   let    inputCountry = event.target.value.trim();
 
-    if (inputCountry === '') {clearMarkup();
-    return;}
+    if (!inputCountry !== '') {clearMarkup();
+    return;} else {
+        fetchCountries(inputCountry)
+        .then(countries => {
+            if (countries.length === 1) {
+                clearMarkup();
+                createContryCard(countries);
+                return;
+            }
+            if (countries.length <= 10 && countries.length >= 2 ) {
+                clearMarkup();
+                createContrylist(countries);
+                return;
+            }
+            if (countries.length > 10) {
+                clearMarkup();
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+                return;
+            }
+        })
+        .catch(error => {
+            Notiflix.Notify.failure("Oops, there is no country with that name")
+        } );
+    }
+console.log(inputCountry);
 
     
 }
+
+
+function clearMarkup () {
+    refs.info.innerHTML = '';
+    refs.list.innerHTML = '';
+}
+
+
+function  createContrylist(countries) {
+    const countriesListMarkup = countries
+      .map(country => {
+        return `<li class="country-list__item"><img class="country-list__item-img" src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="60" height="40" /><p class="country-list__item-name">${country.name.official}</p></li>`;
+      })
+      .join(' ');
+    refs.list.insertAdjacentHTML('beforeend', countriesListMarkup);
+  }
+  
+  function createContryCard(countries) {
+    const countryCardMarkup = countries
+      .map(country => {
+        return `<div class="flag-and-name">
+        <img
+          src="${country.flags.svg}"
+          alt="Flag of ${country.name.official}"
+          width="60"
+          height="40"
+          class="flag-img"
+        />
+        <p class="country-name">${country.name.official}</p>
+      </div>
+      <ul class="country-info-list">
+        <li class="country-info-item">
+          <p class="country-info-text">Capital:&nbsp</p>
+          <span class="country-info-span">${country.capital}</span>
+        </li>
+        <li class="country-info-item">
+          <p class="country-info-text">Population:&nbsp</p>
+          <span class="country-info-span">${country.population}</span>
+        </li>
+        <li class="country-info-item">
+          <p class="country-info-text">Languages:&nbsp</p>
+          <span class="country-info-span">${Object.values(
+            country.languages
+          )}</span>
+        </li>
+      </ul>`;
+      })
+      .join(' ');
+    refs.info.innerHTML = countryCardMarkup;
+  }
