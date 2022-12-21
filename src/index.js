6ec21eceb63f7d5c1a75cc9ce9b52e99f9f35c94
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-21 20:48:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const refs = {
+    input: document.querySelector("#search-box"),
+    list: document.querySelector(".country-list"),
+    info: document.querySelector(".counrty-info"),
+}
+
 
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(name) {
+    return fetch("https://restcountries.com/v3.1/name/${name}?fields=name,capital,currencies,population,flags,lenguages").then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+}
+
+refs.info.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput (event){
+    event.preventDefault();
+
+    inputCountry = event.target.value.trim();
+
+    if (inputCountry === '') {clearMarkup();
+    return;}
+
+    
+}
