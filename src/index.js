b6dea46ea10ee810a416cc0347d83662f74adfdf
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-22 03:58:51 +0200

    end

--- src/index.js
+++ src/index.js
@@ -1,107 +1,119 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+// const lili = document.querySelector(".list");
+
+// lili.style.listStyle = none;
 
 const refs = {
-    input: document.querySelector("#search-box"),
-    list: document.querySelector(".country-list"),
-    info: document.querySelector(".counrty-info"),
-}
-
+  input: document.querySelector('input#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,currencies,population,flags,lenguages`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    })
-}
+const CLEAR = '';
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+// refs.list.
 
-function onInput (event){
-    event.preventDefault();
+function onInput(event) {
+  event.preventDefault();
 
-   let    inputCountry = event.target.value.trim();
+  const inputCountry = event.target.value.trim();
 
-    if (!inputCountry !== '') {clearMarkup();
-    return;} else {
-        fetchCountries(inputCountry)
-        .then(countries => {
-            if (countries.length === 1) {
-                clearMarkup();
-                createContryCard(countries);
-                return;
-            }
-            if (countries.length <= 10 && countries.length >= 2 ) {
-                clearMarkup();
-                createContrylist(countries);
-                return;
-            }
-            if (countries.length > 10) {
-                clearMarkup();
-                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-                return;
-            }
-        })
-        .catch(error => {
-            Notiflix.Notify.failure("Oops, there is no country with that name")
-        } );
-    }
-console.log(inputCountry);
+  // clearMarkup();
 
-    
-}
-
-
-function clearMarkup () {
-    refs.info.innerHTML = '';
-    refs.list.innerHTML = '';
-}
-
-
-function  createContrylist(countries) {
-    const countriesListMarkup = countries
-      .map(country => {
-        return `<li class="country-list__item"><img class="country-list__item-img" src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="60" height="40" /><p class="country-list__item-name">${country.name.official}</p></li>`;
-      })
-      .join(' ');
-    refs.list.insertAdjacentHTML('beforeend', countriesListMarkup);
+  if (!inputCountry) {
+    return;
   }
-  
-  function createContryCard(countries) {
-    const countryCardMarkup = countries
-      .map(country => {
-        return `<div class="flag-and-name">
+
+  fetchCountries(inputCountry)
+    .then(countries => {
+      console.log(countries);
+      const countriesLength = countries.length;
+      console.log(countriesLength)
+ if (countriesLength === 1) {
+  clearMarkup();
+        const markupCard = createContryCard(countries);
+        console.log(markupCard); // alert('1');
+        refs.info.insertAdjacentHTML('beforeend', markupCard);
+       
+        return;
+      }
+      if (countriesLength >= 2 && countriesLength <= 10) {
+        clearMarkup();
+        const markup = createContryList(countries);
+        refs.list.insertAdjacentHTML('beforeend', markup);
+        return;
+      }
+     
+      if (countriesLength > 10) {
+        clearMarkup()
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+    })
+    .catch(error => {
+      clearMarkup();
+      Notiflix.Notify.failure(`Oops, there is no country with that name${error.status}`);
+    });
+}
+
+function clearMarkup() {
+  refs.list.innerHTML = CLEAR;
+  refs.info.innerHTML = CLEAR;
+}
+
+
+
+function createContryList(countries = []) {
+  return countries
+    .map(
+      country =>
+        `<li class="country-list__item list">
+         <img class="country-list__flags" src="${country.flags.svg}" alt="Flag of " width="80" height="60" />
+        <p class="country-list__name-official">${country.name.official}</p>
+        
+       
+       
+       </li>`
+    )
+    // .join('');
+  // refs.list.insertAdjacentHTML('beforeend', countryListMarkup);
+}
+
+function createContryCard(countries = []) {
+  return countries
+    .map(
+      country =>
+        `<div class="flag-and-name">
         <img
           src="${country.flags.svg}"
           alt="Flag of ${country.name.official}"
           width="60"
           height="40"
-          class="flag-img"
+          class="flags"
         />
         <p class="country-name">${country.name.official}</p>
       </div>
       <ul class="country-info-list">
-        <li class="country-info-item">
+        <li class="country-info-item list">
           <p class="country-info-text">Capital:&nbsp</p>
-          <span class="country-info-span">${country.capital}</span>
+          <p class="country-list__capital">${country.capital}</p>
         </li>
-        <li class="country-info-item">
+        <li class="country-info-item list">
           <p class="country-info-text">Population:&nbsp</p>
-          <span class="country-info-span">${country.population}</span>
+          <p class="country-list__population">${country.population}</p> 
         </li>
-        <li class="country-info-item">
+        <li class="country-info-item list">
           <p class="country-info-text">Languages:&nbsp</p>
-          <span class="country-info-span">${Object.values(
-            country.languages
-          )}</span>
+          <p class="country-list__languages">${Object.values(country.languages)}</p>
         </li>
-      </ul>`;
-      })
-      .join(' ');
-    refs.info.innerHTML = countryCardMarkup;
-  }
+      </ul>`
+    )
+    .join('');
+}
